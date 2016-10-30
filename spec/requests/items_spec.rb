ca931cf6adb0560f771091c27bd858aaf8fd041a
andela-megwali <michael.egwali@andela.com>
2016-10-30 12:49:57 +0100

    correct item request spec errors

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -6,84 +6,87 @@ RSpec.describe "Items", type: :request do
 
   describe "POST #create" do
     context "with valid parameters" do
-      it "creates a new user" do
-        post users_path, user: attributes_for(:user)
+      it "creates a new item" do
+        post "/api/v1/bucketlists/1/items", { item: attributes_for(:item) }, auth_header
         json_response = JSON.parse(response.body, symbolize_names: true)
         expect(response).to have_http_status(:success)
-        expect(User.count).to eq 1
-        expect(json_response[:firstname]).to eq "TJ"
-        expect(json_response[:email]).to eq "ex@ex.com"
+        expect(Item.count).to eq 1
+        expect(json_response[:id]).to eq 1
+        expect(json_response[:name]).to eq "MyItems"
+        expect(json_response[:done]).to eq false
       end
     end
 
     context "with invalid parameters" do
-      it "fails to create a new user" do
-        post users_path, user: { firstname: nil }
+      it "fails to create a new item" do
+        create :bucketlist
+        post "/api/v1/bucketlists/1/items", { item: { name: nil } } , auth_header
         json_response = JSON.parse(response.body, symbolize_names: true)
         expect(response).to have_http_status(:success)
-        expect(User.count).to eq 0
-        expect(json_response[:firstname]).to eq nil
-        expect(json_response[:error]).to eq "User not created try again"
+        expect(Item.count).to eq 0
+        expect(json_response[:name]).to_not eq "MyItems"
+        expect(json_response[:error]).to eq "Item not created try again"
       end
+    end
+  end
+
+  describe "GET #index" do
+    before { create :item }
+    it "lists all items in the selected bucketlist" do
+      get "/api/v1/bucketlists/1/items", {}, auth_header
+      json_response = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to have_http_status(:success)
+      expect(json_response.first[:name]).to eq "MyItems"
+      expect(json_response.count).to eq Item.count
     end
   end
 
   describe "GET #show" do
-    before { create :user }
-    context "when user is authorized" do
-      it "renders the selected user" do
-        get user_path(1), {}, auth_header
-        json_response = JSON.parse(response.body, symbolize_names: true)
-        expect(response).to have_http_status(:success)
-        expect(json_response[:firstname]).to eq "TJ"
-        expect(json_response[:id]).to eq 1
-        expect(json_response[:id]).to eq User.first.id
-      end
-    end
-
-    context "when user is not authorized" do
-      it "returns unauthorized error message" do
-        get user_path(1), {}, nil
-        json_response = JSON.parse(response.body, symbolize_names: true)
-        expect(response).to have_http_status(:unauthorized)
-        expect(json_response[:error]).to eq "Not Authorized"
-      end
+    before { create :item }
+    it "renders the selected item" do
+      get "/api/v1/bucketlists/1/items/1", {}, auth_header
+      json_response = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to have_http_status(:success)
+      expect(json_response[:name]).to eq "MyItems"
+      expect(json_response[:id]).to eq 1
+      expect(json_response[:id]).to eq Item.first.id
     end
   end
 
   describe "PUT #update" do
-    before { create :user }
+    before { create :item }
     context "with valid parameters" do
-      it "updates selected user" do
-        put user_path(1), { user: { firstname: "Taris", password: "1234567" } }, auth_header
+      it "updates selected item" do
+        put "/api/v1/bucketlists/1/items/1", { item: { name: "Taris" } }, auth_header
         json_response = JSON.parse(response.body, symbolize_names: true)
         expect(response).to have_http_status(:success)
-        expect(json_response[:firstname]).to eq "Taris"
-        expect(User.first.firstname).to eq "Taris"
+        expect(json_response[:name]).to eq "Taris"
+        expect(Item.first.name).to eq "Taris"
         expect(json_response[:id]).to eq 1
       end
     end
 
     context "with invalid parameters" do
-      it "fails to update selected user" do
-        put user_path(1), { user: { firstname: nil } }, auth_header
+      it "fails to update selected item" do
+        put "/api/v1/bucketlists/1/items/1", { item: { name: nil } }, auth_header
         json_response = JSON.parse(response.body, symbolize_names: true)
         expect(response).to have_http_status(:success)
-        expect(User.first.firstname).to_not eq nil
-        expect(json_response[:error]).to eq "User not updated try again"
+        expect(Item.first.name).to_not eq nil
+        expect(json_response[:name]).to_not eq "MyItems"
+        expect(json_response[:error]).to eq "Item not updated try again"
       end
     end
   end
 
   describe "DELETE #destroy" do
     before { create :item }
-    it "destroys the selected user" do
-      delete user_path(1), {}, auth_header
+    it "destroys the selected item" do
+      delete "/api/v1/bucketlists/1/items/1", {}, auth_header
       json_response = JSON.parse(response.body, symbolize_names: true)
       expect(response).to have_http_status(:success)
-      expect(json_response[:firstname]).to eq nil
-      expect(User.count).to eq 0
-      expect(json_response[:message]).to eq "User deleted"
+      expect(json_response[:name]).to eq nil
+      expect(Item.count).to eq 0
+      expect(json_response[:message]).to eq "Item deleted"
     end
   end
 end
