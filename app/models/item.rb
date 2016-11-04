34423d9eac7f2406dc813fe9c730d89fdd29d76f
andela-megwali <michael.egwali@andela.com>
2016-11-04 12:49:11 +0100

    Update item model to handle nil value

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,12 @@ class Item < ActiveRecord::Base
   belongs_to :bucketlist
 
   validates_presence_of :name, :bucketlist_id
+
+  before_create :item_status
+
+  private
+
+  def item_status
+    self.done ||= 0
+  end
 end
